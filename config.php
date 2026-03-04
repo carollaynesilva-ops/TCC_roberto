2e4bd48f92ebeda736aa8763054ea1c9bb10a239
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2026-03-04 16:50:23 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost","root","","almafisio");
+$conn = new mysqli("localhost","root","3307","almafisio");
 
 if($conn->connect_error){
     die("Erro: " . $conn->connect_error);
